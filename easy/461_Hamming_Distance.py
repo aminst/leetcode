215e39f196bcb9960fcdbedbcd3164164da17f95
aminst <aminst79@gmail.com>
2023-12-11 16:55:19 -0500

    461_Hamming_Distance

---
+++ easy/461_Hamming_Distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        count = 0
+        while xor != 0:
+            if xor % 2 == 1:
+                count += 1
+            xor = xor >> 1
+        return count
