343c34d82a0b58e5ab2313db0d11460163247295
aminst <aminst79@gmail.com>
2024-01-03 10:32:12 -0500

    297 Serialize and Deserialize Binary Tree

--- hard/297_Serialize_and_Deserialize_Binary_Tree.py
+++ hard/297_Serialize_and_Deserialize_Binary_Tree.py
@@ -24,3 +24,51 @@ class Codec:
         add(root, vals)
         return ' '.join(vals)
         
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        
+        def read(vals):
+            cur = next(vals)
+            if cur == "$":
+                return None
+            node = TreeNode(int(cur))
+            node.left = read(vals)
+            node.right = read(vals)
+            return node
+
+        vals = iter(data.split())
+        root = read(vals)
+        return root
+        
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+"""
+Question:
+    * unique: no
+    * binary tree
+
+BFS:
+1
+2 3
+4 5
+how to know nothing?
+
+track how many we added for that parent
+store if we have added left or right of parent
+
+
+Format:
+1 2 3 null null 4 5
+
+Add null to queue?
+
+"""
